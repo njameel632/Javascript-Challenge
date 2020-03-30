d48f32f49a79a7aa9d999b033f8ce4f9f4bab709
njameel632 <57690530+njameel632@users.noreply.github.com>
2020-03-29 20:03:07 -0400

    adding files

---
+++ UFO-Level-2/static/js/app.js
@@ -0,0 +1,30 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+const tbody = d3.select("tbody");
+
+function buildTable(ufos) {
+  tbody.html("");
+
+  ufos.forEach(datarow => {
+    const row = tbody.append("tr");
+
+    Object.values(datarow).forEach(val => {
+      let cell = row.append("td");
+      cell.text(val);
+    });
+  });
+}
+buildTable(tableData);
+
+// Creating a Function to take input date and filter as entries as per input
+function handleClick() {
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    var filtereddate = tableData.filter(sighting => sighting.datetime === inputValue);
+    buildTable(filtereddate);
+}
+d3.selectAll("#filter-btn").on("click", handleClick);
